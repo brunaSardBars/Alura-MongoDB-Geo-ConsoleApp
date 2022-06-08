f9e30e4dc5044c7c364c5bdce1c621af2db5d7e6
brunasa <brunasa@synnex.com>
2022-06-08 18:11:01 -0300

    Add project files.

---
+++ ProjetoAeroportos/Airport.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAeroportos
+{
+    public class Airport
+    {
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        public GeoJsonPoint<GeoJson2DGeographicCoordinates> loc { get; set; }
+
+        public string name { get; set; }
+        public string type { get; set; }
+        public string code { get; set; }
+
+    }
+   
+}
---
+++ ProjetoAeroportos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver.GeoJsonObjectModel;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace ProjetoAeroportos
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Iniciando...");
+            await MainAsync(args);
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var connection = new ConnectionMongoDB();
+
+            var point = new GeoJson2DGeographicCoordinates(-118.32528, 34.103212); 
+            var location = new GeoJsonPoint<GeoJson2DGeographicCoordinates>(point);
+
+            var constructor = Builders<Airport>.Filter;
+            var filter = constructor.NearSphere(x => x.loc, location, 100000);
+
+            var airports = await connection.Airports.Find(filter).ToListAsync();
+            foreach (var airport in airports)
+            {
+                Console.WriteLine(airport.ToJson<Airport>());
+            }
+            Console.WriteLine("");
+        }
+    }
+}
